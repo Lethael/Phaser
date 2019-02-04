55caca399f2f044a55e27ff3d7373da50cc30d65
Lethael <benjamin.desfontaines@gmail.com>
2019-02-04 16:14:28 +0100

    Add rect to select target, add deleted target when hp <= 0

--- Scenes/BattleScene.js
+++ Scenes/BattleScene.js
@@ -14,22 +14,35 @@ class BattleScene extends Phaser.Scene{
 	}
 	
 	create(){
+        this.cursors = this.input.keyboard.createCursorKeys();
+        // When selecting monster
+        this.targetMonster = 0;
+        //To know which actor can makes an action
         this.indexArray = 0;
         this.timeUntilNextAction = 2000;
+        
+        //Contains all battlers
         this.actorsList = new Array();
         
+        //Init Mobs
         this.listMobs = new Array();
         this.listMobs.push(new Monsters("Witch1", "witch", 1, 1, 1, 0));
         this.listMobs.push(new Monsters("Witch2", "witch", 1, 1, 1, 0));
         this.listMobs.push(new Monsters("Witch3", "witch", 1, 1, 1, 0));
         this.listMobs.push(new Monsters("Witch4", "witch", 1, 1, 1, 0));
+        this.listImg = new Array();
         this.posXMonster = 800 / this.listMobs.length - 40;
         this.posYMonster = 150;
-		this.add.image(0, 0, 'background').setOrigin(0, 0);
+		this.add.image(0, 0, 'background').setOrigin(0, 0).setInteractive();
         for(var i = 0; i < this.listMobs.length; i++){
-            this.add.image(this.posXMonster, this.posYMonster, 'witch');   
+            this.listImg[i] = this.add.image(this.posXMonster, this.posYMonster, 'witch');   
             this.posXMonster += 800 / this.listMobs.length - 40;
         }
+        this.posXMonster = 800 / this.listMobs.length - 40;
+        
+        //end Init Mobs
+        
+        //Display Player's value
         this.rectX = 0;
         this.rectY = 500;
 
@@ -43,21 +56,21 @@ class BattleScene extends Phaser.Scene{
             this.rectX += 205;
         }
         this.rectX = 0;
-		this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        // end
+        
         this.actorsList = calculInit(this.herotest, this.listMobs);
         
         this.boolEnd = false;
+        
+        // Rectangle to select target Monster
+        this.graphics = this.add.graphics({lineStyle: {width: 2, color: 0xffffff}});
+        this.rects = new Phaser.Geom.Rectangle(this.listImg[0].x - 40, 75, 80, 140);
         
 	}
 	
 	update(time, delta){
         /* Only to test */
-		/*if(Phaser.Input.Keyboard.JustDown(this.spacebar)){
-            //this.herotest[0].attackMonster(this.listMobs[0]);
-            this.listMobs[0].attackHero(this.herotest[0]);
-			//this.herotest[0].attribute.life -= 1;
-			//this.lifeText[0].setText("HP :" + this.herotest[0].attribute.life + ' / ' + this.herotest[0].attribute.maxLife);
-		}
+		/*
 		if(this.herotest[0].attribute.life <= 0){
             this.herotest[0].attribute.life = this.herotest[0].attribute.maxLife
             this.scene.start('MapTest', {heros: this.herotest});
@@ -71,8 +84,33 @@ class BattleScene extends Phaser.Scene{
             if(this.actorsList[this.indexArray].constructor.name === "Monsters"){
                 this.timeUntilNextAction -= delta;
             }else{
-                console.log("Player !");
-                this.indexArray += 1;
+                this.graphics.strokeRectShape(this.rects);
+                if(Phaser.Input.Keyboard.JustDown(this.cursors.space)){
+                    this.graphics.clear();
+                    this.actorsList[this.indexArray].attackMonster(this.listMobs[this.targetMonster]);
+                    this.indexArray += 1;
+                    if(this.listMobs[this.targetMonster].life <= 0){
+                        for(var j = 0; j < this.actorsList.length; j++){
+                            if(this.listMobs[this.targetMonster] === this.actorsList[j]){
+                                this.actorsList.splice(j, 1);
+                            }
+                        }
+                        this.listImg[this.targetMonster].destroy();
+                        this.listMobs.splice(this.targetMonster, 1);
+                        this.listImg.splice(this.targetMonster, 1);
+                        this.targetMonster = 0;
+                    }
+                }else if(Phaser.Input.Keyboard.JustDown(this.cursors.right)){
+                    
+                    console.log(this.rects.x)
+                    this.targetMonster += 1;
+                    this.graphics.clear();
+                    if(this.rects.x >= this.listImg[this.listMobs.length - 1].x - 40){
+                        this.targetMonster = 0;
+                        this.rects.x = this.listImg[0] - 40;
+                    }
+                    this.rects.x = this.listImg[this.targetMonster].x - 40;
+                }
             }
             if(this.timeUntilNextAction <= 0){
                 monsterTurn(this.herotest, this.actorsList[this.indexArray]);
@@ -80,12 +118,29 @@ class BattleScene extends Phaser.Scene{
                 this.timeUntilNextAction = 2000;
             }
             
-            if(this.indexArray == this.actorsList.length)
+            if(this.indexArray >= this.actorsList.length)
                 this.boolEnd = true;
         }
 	}
     
 }
+    
+/*function displayMonster(imgs, monsters, imgMob, battle){
+    battle.posXMonster = 800 / monsters.length - 40;
+    battle.posYMonster = 150;
+    for(var i = 0; i < monsters.length; i++){
+        imgs[i] = battle.add.sprite(battle.posXMonster, battle.posYMonster, imgMob);   
+        battle.posXMonster += 800 / monsters.length - 40;
+        }
+    battle.posXMonster = 800 / battle.listMobs.length - 40;
+}*/
+
+/*
+    @params Heros list
+    @params Monsters list
+    
+    return list of all of them sort by intiative
+*/
 function calculInit(herotest, listMobs){
     var newInit = new Array();
     for(var i = 0; i < herotest.length; i++){
@@ -105,6 +160,7 @@ function calculInit(herotest, listMobs){
     newInit.reverse();
     return newInit;
 }
+
 
 function monsterTurn(herotest, monster){
     console.log(monster.name + " attack !");
