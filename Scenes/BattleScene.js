b0598908192c0da5c398f4c62b2621378ca3b1d9
Lethael <benjamin.desfontaines@gmail.com>
2019-02-05 11:07:31 +0100

    change if by switch when select action value

--- Scenes/BattleScene.js
+++ Scenes/BattleScene.js
@@ -102,28 +102,53 @@ class BattleScene extends Phaser.Scene{
                 }
                     
                 if(Phaser.Input.Keyboard.JustDown(this.cursors.space)){
-                    if(this.posCursor === 0){
-                        this.graphicsRectTargetMonster.clear();
-                        if(this.damageText != undefined)
-                            this.damageText.destroy();
+                    switch(this.posCursor){
+                        case 0:
+                            this.graphicsRectTargetMonster.clear();
+                            if(this.damageText != undefined)
+                                this.damageText.destroy();
 
-                        this.damageText = this.add.text(395, 40, this.actorsList[this.indexArray].attackMonster(this.listMobs[this.targetMonster]), {fontSize: '20px', fill: '#FFF'});
-                        this.indexArray += 1;
-                        if(this.listMobs[this.targetMonster].life <= 0){
-                            for(var j = 0; j < this.actorsList.length; j++){
-                                if(this.listMobs[this.targetMonster] === this.actorsList[j]){
-                                    this.actorsList.splice(j, 1);
+                            this.damageText = this.add.text(395, 40, this.actorsList[this.indexArray].attackMonster(this.listMobs[this.targetMonster]), {fontSize: '20px', fill: '#FFF'});
+                            this.indexArray += 1;
+                            if(this.listMobs[this.targetMonster].life <= 0){
+                                for(var j = 0; j < this.actorsList.length; j++){
+                                    if(this.listMobs[this.targetMonster] === this.actorsList[j]){
+                                        this.actorsList.splice(j, 1);
+                                    }
                                 }
+                                this.destroyMobs();
+                                this.targetMonster = 0;
+                                if(this.listMobs.length === 0)
+                                    this.scene.start('MapTest', {heros: this.herotest});
                             }
-                            this.destroyMobs();
-                            this.targetMonster = 0;
-                            if(this.listMobs.length === 0)
-                                this.scene.start('MapTest', {heros: this.herotest});
-                        }
-                        this.inAction = false;
-                        this.destroyMenuBattle();
+                            this.inAction = false;
+                            this.destroyMenuBattle();
+                        break;
+                        case 1:
+                            if(this.damageText != undefined)
+                                    this.damageText.destroy();
+
+                                this.damageText = this.add.text(395, 40, "Not implemented yet...", {fontSize: '20px', fill: '#FFF'});
+                        break;
+                            case 2:
+                            if(this.damageText != undefined)
+                                    this.damageText.destroy();
+
+                                this.damageText = this.add.text(395, 40, "Not implemented yet...", {fontSize: '20px', fill: '#FFF'});
+                        break;
+                            case 3:
+                            if(this.damageText != undefined)
+                                    this.damageText.destroy();
+
+                                this.damageText = this.add.text(395, 40, "Not implemented yet...", {fontSize: '20px', fill: '#FFF'});
+                        break;
+                            case 4:
+                            if(this.damageText != undefined)
+                                    this.damageText.destroy();
+
+                                this.damageText = this.add.text(395, 40, "Not implemented yet...", {fontSize: '20px', fill: '#FFF'});
+                        break;
                     }
-                    
                 }else if(Phaser.Input.Keyboard.JustDown(this.cursors.right)){
                     this.targetMonster += 1;
                     this.graphicsRectTargetMonster.clear();
