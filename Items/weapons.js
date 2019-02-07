4cf1910cf3360ea099344e34c05a80ddcfed7fc6
Lethael <benjamin.desfontaines@gmail.com>
2019-02-07 11:20:11 +0100

    add baseBonusDamage attribute and add calculBonusByDurability method (empty)

--- Items/weapons.js
+++ Items/weapons.js
@@ -3,7 +3,12 @@ class Weapon extends Items{
         super(type, name, description);
         this.diceDamage = diceDamage;
         this.bonusDamage = bonusDamage;
+        this.baseBonusDamage = bonusDamage;
         this.size = size;
         this.durability = durability;
     }
+}
+
+Weapon.prototype.calculBonusByDurability = function(){
+    
 }
