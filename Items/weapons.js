268f823d2b421a266f4c9b7af0e903a316564c66
Lethael <benjamin.desfontaines@gmail.com>
2019-02-07 11:41:27 +0100

    calculBonusByDurability is now functionnal

--- Items/weapons.js
+++ Items/weapons.js
@@ -6,9 +6,24 @@ class Weapon extends Items{
         this.baseBonusDamage = bonusDamage;
         this.size = size;
         this.durability = durability;
+        this.calculBonusByDurability();
     }
 }
 
 Weapon.prototype.calculBonusByDurability = function(){
+    if(this.durability <= 50){
+        let looseBonus = Math.floor((50 - this.durability) / 10);
+        this.bonusDamage -= looseBonus;
+        console.log("loose bonus : " + this.bonusDamage);
+    }
+    else if(this.durability > 50 && this.durability <= 79){
+        this.bonusDamage = this.baseBonusDamage;
+        console.log("Between 51 & 79");
+    }
+    else if(this.durability > 79){
+        let winBonus = Math.floor((this.durability - 50) / 10);
+        this.bonusDamage += winBonus;
+        console.log("win bonus : " + this.bonusDamage);
+    }
     
 }
